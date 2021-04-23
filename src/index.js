b70e4ad83ee553a6cb6a775c817c481020ea4895
Eylül Güneş <eylulgist@gmail.com>
2021-04-23 22:15:19 +0300

    added background + created sprite

--- src/index.js
+++ src/index.js
@@ -22,12 +22,20 @@ function preload() {
   //contains usable functions and properties
   //need to specify 2 values: key, file path
   this.load.image("sky-background", "assets/sky.png");
+  this.load.image("bird", "assets/bird.png");
 }
+
+let bird = null;
 
 //Initializing instances of the objects
 function create() {
   //need to specify 3 values: x-coordinate, y-coordinate, key
-  this.add.image(0, 0, "sky-background");
+  //this.add.image(config.width / 2, config.height / 2, "sky-background");
+  this.add.image(0, 0, "sky-background").setOrigin(0);
+  bird = this.add
+    .sprite(config.width * 0.1, config.height / 2, "bird")
+    .setOrigin(0);
+  console.log(bird.body);
 }
 
 new Phaser.Game(config);
